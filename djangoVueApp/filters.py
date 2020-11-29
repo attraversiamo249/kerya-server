b70c8d127b0469ed118c0d4fae5bb159e094523a
attraversiamo249 <hichemdahi57@gmail.com>
2020-11-29 11:43:06 +0100

    filters

--- djangoVueApp/filters.py
+++ djangoVueApp/filters.py
@@ -2,9 +2,18 @@ from django_filters import rest_framework as filters
 from .models import House
 
 class HouseFilter(filters.FilterSet):
-    type = filters.TypedMultipleChoiceFilter(field_name="type",lookup_expr="exact")
-    max_price = filters.NumberFilter(field_name="price",lookup_expr="lte")
-    rooms = filters.TypedMultipleChoiceFilter(field_name="type",lookup_expr="exact")
+    ChoicesType=(
+        ("app","Appartement"),
+        ("villa","Villa")
+    )
+    ChoicesRooms=(
+        ("app","Appartement"),
+        ("villa","Villa")
+    )
+
+    type = filters.MultipleChoiceFilter(field_name="type",lookup_expr="exact",required=False,choices=ChoicesType)
+    max_price = filters.NumberFilter(field_name="price",lookup_expr="lte",required=False)
+    rooms = filters.MultipleChoiceFilter(field_name="type",lookup_expr="exact",required=False,choices=ChoicesRooms)
 
     class Meta: 
         model = House
