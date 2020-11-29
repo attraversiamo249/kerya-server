8d57f64e079eb17982c6453c374cfba8d9b17e31
attraversiamo249 <hichemdahi57@gmail.com>
2020-11-29 11:04:10 +0100

    filters

--- djangoVueApp/filters.py
+++ djangoVueApp/filters.py
@@ -2,12 +2,10 @@ from django_filters import rest_framework as filters
 from .models import House
 
 class HouseFilter(filters.FilterSet):
-    min_rooms = filters.NumberFilter(field_name="rooms", lookup_expr='gte')
-    max_rooms = filters.NumberFilter(field_name="rooms", lookup_expr='lte')
-    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
-    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
     type = filters.AllValuesMultipleFilter(field_name="type",lookup_expr="exact")
+    max_price = filters.NumberFilter(field_name="price",lookup_expr="lte")
+    rooms = filters.AllValuesMultipleFilter(field_name="type",lookup_expr="exact")
 
     class Meta: 
         model = House
-        fields = ['city','type','address','min_rooms','min_rooms','min_price','max_price']
+        fields = ['city','type','address','rooms','max_price']
