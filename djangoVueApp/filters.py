c11ba4c0ab9169dcfa76e1af73e682cc58ce87b7
attraversiamo249 <hichemdahi57@gmail.com>
2020-12-03 11:08:40 +0100

    multi filter

--- djangoVueApp/filters.py
+++ djangoVueApp/filters.py
@@ -5,7 +5,8 @@ class HouseFilter(filters.FilterSet):
     ChoicesType=(
         ("Appartement","Appartement"),
         ("Villa","Villa"),
-        ("Studio","Studio")
+        ("Studio","Studio"),
+        ("Chambre colocation","Chambre colocation")
 
     )
     ChoicesRooms=(
@@ -17,7 +18,7 @@ class HouseFilter(filters.FilterSet):
     type = filters.MultipleChoiceFilter(field_name="type",lookup_expr="exact",required=False,choices=ChoicesType)
     max_price = filters.NumberFilter(field_name="price",lookup_expr="lte",required=False)
     rooms = filters.MultipleChoiceFilter(field_name="rooms",lookup_expr="exact",required=False,choices=ChoicesRooms)
-
+    rooms_gte= filters.NumberFilter(field_name="rooms",lookup_expr="gte",required=False)
     class Meta: 
         model = House
-        fields = ['city','type','address','rooms','max_price']
+        fields = ['city','type','address','rooms','max_price','rooms_gte']
