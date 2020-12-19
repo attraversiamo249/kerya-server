75c7582034702c42db8f85713f99979a570ce381
attraversiamo249 <hichemdahi57@gmail.com>
2020-12-19 18:47:03 +0100

    ssl commit

--- djangoVue/settings.py
+++ djangoVue/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'djangoVue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
             }
 }
 import dj_database_url
