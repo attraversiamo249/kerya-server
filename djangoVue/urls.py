294d5451b597817910c59571bf9bff2dcc942eff
attraversiamo249 <hichemdahi57@gmail.com>
2020-12-29 11:42:16 +0100

    social intgeration

--- djangoVue/urls.py
+++ djangoVue/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
+    path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
     path('admin/', admin.site.urls),
     path('api/', include('djangoVueApp.urls')),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
