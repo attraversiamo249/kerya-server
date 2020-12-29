3a9ed7f6038f6e67572253f33db0599e4ad894ad
attraversiamo249 <hichemdahi57@gmail.com>
2020-12-29 11:40:21 +0100

    social intgeration

--- djangoVue/settings.py
+++ djangoVue/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django_filters',
     'oauth2_provider',
     'social_django',
-    'rest_framework_social_oauth2',
+    'drf_social_oauth2',
 ]
 
 MIDDLEWARE = [
@@ -99,17 +99,6 @@ FACEBOOK_EXTENDED_PERMISSIONS = ['email']
 SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'email']
 SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
 
-SOCIAL_AUTH_PIPELINE = (
-'social_core.pipeline.social_auth.social_details',
-'social_core.pipeline.social_auth.social_uid',
-'social_core.pipeline.social_auth.auth_allowed',
-'social_core.pipeline.social_auth.social_user',
-'social_core.pipeline.user.get_username',
-'social_core.pipeline.social_auth.associate_by_email',
-'social_core.pipeline.user.create_user',
-'social_core.pipeline.social_auth.associate_user',
-'social_core.pipeline.social_auth.load_extra_data',
-'social_core.pipeline.user.user_details', )
 
 
 WSGI_APPLICATION = 'djangoVue.wsgi.application'
