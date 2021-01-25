813841eefcff15bc5a62594e736ff5eb2c2dc7c5
attraversiamo249 <hichemdahi57@gmail.com>
2021-01-25 12:42:33 +0100

    social intgeration

--- djangoVueApp/models.py
+++ djangoVueApp/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+     picture =models.ImageField()
      email = models.EmailField(verbose_name='email',max_length=255, unique=True)
      phone = models.CharField(null=True, max_length=255)
      REQUIRED_FIELDS =['username','phone','first_name','last_name']
